e45e890ae0f5789b6425cad308f5d7756ca60558
Fer Aguirre <77854667+fer-aguirre@users.noreply.github.com>
2025-04-12 17:43:15 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Musica Arg NLP',
+    version='0.1.0',
+    author='Fernanda Aguirre Ruiz',
+    description='A template for data analysis projects using python',
+    python_requires='>=3',
+    license='MIT License',
+    packages=find_packages(),
+)
